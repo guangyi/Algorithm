71f328b0f79403ec7d6fad7ba22a96275317cbe2
guangyi <zhouguangyi2009@gmail.com>
2014-05-25 09:55:22 -0400

    merge K sorted Array works

--- mergeKsortedArray.py
+++ mergeKsortedArray.py
@@ -1,7 +1,7 @@
 '''
 Created on May 24, 2014
 
-Merge K sorted Array-- with Heap: (arr[i], (arr, i))
+Merge K sorted Array-- with Heap: (arr[i], (indexOfArray, elementIndexInArray))
 
 @author: zhouguangyi2009
 '''
@@ -10,9 +10,28 @@ class Solution:
     # @param arr: a collection of k arr 
     def mergeKsortedArray(self, arr):
         k = len(arr)
+        result = []
         heap = []
         for i in range(0, k):
-            heap.append((arr[i][0],(arr[i])))
+            heap.append((arr[i][0],(i, 0)))
         # transform list heap into a heap
         heapq.heapify(heap)
-            
+        
+        while len(heap) != 0:
+            print len(heap)
+            min = heapq.heappop(heap)
+            # data structure: (value, (arrIndex, elemIndex))
+            arrTup = min[1]
+            arrIndex = arrTup[0]
+            elemIndex = arrTup[1]
+            result.append(min[0])
+            elemIndex += 1
+            if elemIndex < len(arr[arrIndex]):
+                heapq.heappush(heap, (arr[arrIndex][elemIndex],(arrIndex, elemIndex)))
+        return result
+arr = [
+       [1,2,9],
+       [0,2,3,4,],
+       [5,6]
+       ] 
+print Solution().mergeKsortedArray(arr)
