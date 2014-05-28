1136a0096f29cbcc4027aa18fa5a3114ad4bd42b
guangyi <zhouguangyi2009@gmail.com>
2014-05-28 16:50:33 -0400

    check if a binary tree is balanced-- modified with less code

--- isbalanced.py
+++ isbalanced.py
@@ -3,6 +3,11 @@ Created on May 17, 2014
 
 @author: zhouguangyi2009
 '''
+'''
+Created on May 17, 2014
+
+@author: zhouguangyi2009
+'''
 class tree():
     def __init__(self, val, left, right):
         self.val = val
@@ -13,29 +18,21 @@ class Solution:
     # @param root, a tree node
     # @return a boolean
     def isBalanced(self, root):
-        if root == None: return True
-        else:
-            maxHeight = self.isBalance(root)
-            print maxHeight
-            if maxHeight != False:
-                return True
-            else: return False
-    
+        height = self.isBalance(root)
+        if height != False:
+            return True
+        return height
     def isBalance(self, root):
         if root == None:
-            return 0
+            return True
         left = self.isBalance(root.left)
+        if left == False: return False
         right = self.isBalance(root.right)
-        if left is False or right is False or abs(left - right) > 1: 
-            print "false" 
+        if right == False: return False
+        if abs(left - right) > 1:
             return False
-        elif abs(left - right) <= 1:
-            if left > right:
-                left += 1
-                return left
-            else:
-                right += 1
-                return right
-
+        else:
+            return max(left, right) + 1
+    
 root = tree(1,None, None) 
 print Solution().isBalanced(root)      
--- subSet.py
+++ subSet.py
@@ -36,26 +36,4 @@ print Solution().getAllSubset([])
 print Solution().getAllSubset([2])       
 print Solution().getAllSubset([4,1,0])
 
-    '''
-    Recursion way
-    def subsets(self, Set):
-        allsubSet = []
-        self.recurSet(Set, len(Set),allsubSet)
-        return allsubSet
-    
-    def recurSet(self, Set, index, allsubSet):
-        result = []
-        if index == 0:
-            allsubSet.append([])
-            return [[]]
-        else:
-            subset = self.recurSet(Set, index-1, allsubSet) 
-            result = result + subset
-            for item in result:
-                newSub = item + [Set[index-1]]
-                # single element array sort return None
-                if len(newSub) > 1:
-                    newSub.sort()
-                allsubSet.append(newSub )
-        return allsubSet
-    '''    
+   
