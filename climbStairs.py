f51fed7a6bf20aa89bd6e6d66fca34b12b043594
guangyi <zhouguangyi2009@gmail.com>
2014-05-28 20:39:41 -0700

    climb stairs. start from < 0, and  == 0 with extra arr to improve effitiency

--- climbStairs.py
+++ climbStairs.py
@@ -6,50 +6,27 @@ Created on May 19, 2014
 class Solution:
     # @param n, an integer
     # @return an integer
-    def __init__(self):
-        self.result = 0
-        self.arr = [0,1,2]
-    def climbStairs3(self, n):
-        dp = [0, 1, 2]
-        i = 3
-        while i <= n:
-            dp.append(dp[i-1] + dp[i-2] + dp[i - 3])
-            i += 1
-        return dp[n] 
-    def climbStairs2(self, n, arr):
-        if n < 0:
-            return 0
-        elif n == 0:
-            return 1
-        else:
-            if n < len(arr):
-                return arr[n]
-            else:
-                val = self.climbStairs2(n - 2, arr) + self.climbStairs2(n - 1, arr)
-                print arr
-                arr.append(val)
-                return arr[n]
-    def climbStairs4(self, n):
-        if n < 0:
-            return 0
-        elif n == 0:
-            return 1
-        else:
-            if n < len(self.arr):
-                return self.arr[n]
-            else:
-                val =self.climbStairs4(n - 3) + self.climbStairs4(n - 2) + self.climbStairs4(n - 1)
-                self.arr.append(val)
-                return self.arr[n]
+    ''' no memorization not effitiency
     def climbStairs(self, n):
-        if n < 0:
+        if n < 0 :
             return 0
-        elif n == 0:
+        if n == 0:
             return 1
-        else:
-            return self.climbStairs(n - 1) + self.climbStairs (n - 2)
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+    '''
+    def __init__(self):
+        # becuase need to return arr[n]
+        # n couldn't be 0, so arr starts from 0
+        self.arr = [0, 1, 2]
     
-arr= [0,1,2]
-#print Solution().climbStairs2(35, arr)
-print Solution().climbStairs4(35)
-#print Solution().climbStairs3(3)
+    def climbStairs(self, n):
+        if n < 0 :
+            return 0
+        if n == 0:
+            return 1
+        if n < len(self.arr):
+            return self.arr[n]
+        else:
+            self.arr.append( self.climbStairs(n - 1) + self.climbStairs(n - 2) )
+            return self.arr[n]
+            
